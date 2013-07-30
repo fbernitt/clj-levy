721f6f2aefb787ba9507ee7b88069d0bd4d99e3d
Folker Bernitt <folker-bernitt-github@gmx.de>
2013-07-30 21:52:55 +0200

    Improved test cases for levenshtein and damerau-levenshtein.

--- test/clj_levy/core_test.clj
+++ test/clj_levy/core_test.clj
@@ -7,14 +7,22 @@
   (is (= 3 (levenshtein "foo" "")))
   (is (= 6 (levenshtein "" "foobar")))
   (is (= 1 (levenshtein "a" "b")))
-  (is (= 2 (levenshtein "ab" "bc"))))
+  (is (= 2 (levenshtein "ab" "bc")))
+  (is (= 2 (levenshtein "ab" "ba")))
+  (is (= 3 (levenshtein "foo" "bar")))
+  (is (= 2 (levenshtein "lein" "lien")))
+  (is (= 4 (time (levenshtein "levenshtein" "meilenstein")))))
 
 (deftest test-damerau-levenshtein
   (is (= 0 (damerau-levenshtein "foo" "foo")))
-  (is (= 1 (damerau-levenshtein "foo" "ofo")))
+  (is (= 3 (damerau-levenshtein "foo" "")))
+  (is (= 6 (damerau-levenshtein "" "foobar")))
+  (is (= 1 (damerau-levenshtein "a" "b")))
+  (is (= 2 (damerau-levenshtein "ab" "bc")))
+  (is (= 1 (damerau-levenshtein "ab" "ba")))
   (is (= 3 (damerau-levenshtein "foo" "bar")))
-  (is (= 1 (damerau-levenshtein "lein" "lien"))))
-
+  (is (= 1 (damerau-levenshtein "lein" "lien")))
+  (is (= 4 (time (damerau-levenshtein "levenshtein" "meilenstein")))))
 
 (deftest test-keyboard-distance
   (is (= 0 (keyboard-distance "a" "a")))
@@ -23,8 +31,7 @@
   (is (= 1 (keyboard-distance "a" "q")))
   (is (= 2 (keyboard-distance "a" "3")))
   (is (= 12 (keyboard-distance "lein" "lei")))
-  (is (= 4 (keyboard-distance "lein" "kwub")))
-         )
+  (is (= 4 (keyboard-distance "lein" "kwub"))))
 
 (deftest test-keyboard-distance-probability
   (letfn [(round [f] (* 0.01 (Math/round (* 100 f))))]
