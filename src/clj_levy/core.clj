84a934a241407ab7234b3e9f698d2af4cfa40db7
Folker Bernitt <folker-bernitt-github@gmx.de>
2013-07-30 20:18:24 +0200

    Imported distance calculation from local repo.
    - added levenshtein distance
    - added damerau levenshtein distance
    - added keyboard layout distance

--- src/clj_levy/core.clj
+++ src/clj_levy/core.clj
@@ -1,6 +1,108 @@
 (ns clj-levy.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn levenshtein
+  [seq1 seq2]
+  (cond
+    (empty? seq1) (count seq2)
+    (empty? seq2) (count seq1)
+    :else (min
+            (+ (if (= (first seq1) (first seq2)) 0 1)
+               (levenshtein (rest seq1) (rest seq2)))
+            (inc (levenshtein (rest seq1) seq2))
+            (inc (levenshtein seq1 (rest seq2))))))
+
+
+; http://stackoverflow.com/questions/8619785/what-is-an-efficient-way-to-measure-similarity-between-two-strings-levenshtein
+(defn damerau-levenshtein
+  "Calculates the damerau-levenshtein distance of two strings in an iterative way"
+  [seq1 seq2]
+  (let [score (make-array Integer/TYPE (+ 2 (count seq1)) (+ 2 (count seq2)))]
+    (dotimes [row (inc (count seq1))]
+      (aset-int score row 0 row))
+    (dotimes [col (inc (count seq2))]
+      (aset-int score 0 col col))
+    (doseq [y (range 1 (inc (count seq1)))]
+      (doseq [x (range 1 (inc (count seq2)))]
+        (let [cost (if (= (nth seq1 (dec y)) (nth seq2 (dec x))) 0 1)
+              value (min 
+                      (inc (aget score (dec y) x))
+                      (inc (aget score y (dec x)))
+                      (+ cost (aget score (dec y) (dec x))))]
+          ;(println (str x y "=" value " and cost " cost))
+          (aset-int score y x value)
+          ;now the damerau additions
+          (when (and (> x 1) (> y 1))
+            (when (and (= (nth seq1 (dec y)) (nth seq2 (- x 2)))
+                     (= (nth seq1 (- y 2)) (nth seq2 (dec x))))
+              (let [value (min
+                            (aget score y x)
+                            (+ cost (aget score (- y 2) (- x 2))))]
+                (aset-int score y x value))))
+      )))
+    ;(pprint/pprint score)
+    (aget score (count seq1) (count seq2))))
+
+(def qwertz-chars [["^1234567890ß´" "°!\"§$%&/()=?`"]
+                   [" qwertzuiopü+" " QWERTZUIOPÜ*"]
+                   [" asdfghjklöä#" " ASDFGHJKLÖÄ'"]
+                   [" <yxcvbnm,.- " " >YXCVBNM;:_ "]])
+
+(def qwerty-chars [["§1234567890-=" "±!@#$%^&*()_+"]
+                   [" qwertyuiop[]" " QWERTYUIOP{}"]
+                   [" asdfghjkl;'\\" " ASDFGHJKL:\"|"]
+                   [" `zxcvbnm,./ " " ~ZXCVBNM<>? "]])
+
+(defn build-key-map
+  [keyboard-layout]
+  (into {} (reduce into (reduce into 
+    (map-indexed (fn [row buttons-shifted]
+                   (map-indexed (fn [shift buttons]
+                                  (map-indexed (fn [col button] 
+                                                 [button [col row shift]]) buttons))
+                                buttons-shifted))
+                 keyboard-layout)))))
+
+(defn- button-distance
+  [keymap [x1 y1 shift1] [x2 y2 shift2]]
+  (let [x (- x2 x1)
+        y (- y2 y1)]
+    (int (Math/sqrt (+ (* x x) (* y y))))))
+
+(defn- max-distance
+  [keymap]
+  (let [buttons (vals keymap)]
+    (reduce max
+      (flatten
+        (for [b buttons]
+          (map #(button-distance keymap % b) buttons))))))
+
+(defn- char-distance
+  "returns the number of steps between to keys. If no key for char is found max distance is returned"
+  [c1 c2 keymap]
+  (let [b1 (keymap c1)
+        b2 (keymap c2)]
+    (if (some nil? [b1 b2])
+      13
+      (button-distance keymap b1 b2))))
+
+; http://cpansearch.perl.org/src/KRBURTON/String-KeyboardDistance-1.01/KeyboardDistance.pm
+; https://gist.github.com/ck/960716
+(defn keyboard-distance
+  "Returns the distance of two string based on the location of their characters on a keyboard."
+  [seq1 seq2]
+  (let [keymap (build-key-map qwertz-chars)
+        len-dist (* (Math/abs (- (count seq1) (count seq2))) (max-distance keymap))]
+    (letfn [(distance [c1 c2] (char-distance c1 c2 keymap))]
+      (+ (apply + (map distance seq1 seq2)) len-dist)
+      )))
+
+
+(defn keyboard-distance-probability
+  "Returns the probability that two strings are keyboard layout transpositions"
+  [str1 str2]
+  (let [keymap (build-key-map qwertz-chars)
+        short (min (count str1) (count str2))
+        long (max (count str1) (count str2))
+        dist (keyboard-distance str1 str2)]
+        (- 1.0 (/ dist (* long (max-distance keymap))))))
+
--- test/clj_levy/core_test.clj
+++ test/clj_levy/core_test.clj
@@ -2,6 +2,34 @@
   (:require [clojure.test :refer :all]
             [clj-levy.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-levenshtein
+  (is (= 0 (levenshtein "foo" "foo")))
+  (is (= 3 (levenshtein "foo" "")))
+  (is (= 6 (levenshtein "" "foobar")))
+  (is (= 1 (levenshtein "a" "b")))
+  (is (= 2 (levenshtein "ab" "bc"))))
+
+(deftest test-damerau-levenshtein
+  (is (= 0 (damerau-levenshtein "foo" "foo")))
+  (is (= 1 (damerau-levenshtein "foo" "ofo")))
+  (is (= 3 (damerau-levenshtein "foo" "bar")))
+  (is (= 1 (damerau-levenshtein "lein" "lien"))))
+
+
+(deftest test-keyboard-distance
+  (is (= 0 (keyboard-distance "a" "a")))
+  (is (= 1 (keyboard-distance "a" "s")))
+  (is (= 8 (keyboard-distance "a" "l")))
+  (is (= 1 (keyboard-distance "a" "q")))
+  (is (= 2 (keyboard-distance "a" "3")))
+  (is (= 12 (keyboard-distance "lein" "lei")))
+  (is (= 4 (keyboard-distance "lein" "kwub")))
+         )
+
+(deftest test-keyboard-distance-probability
+  (letfn [(round [f] (* 0.01 (Math/round (* 100 f))))]
+  (is (= 1.0 (keyboard-distance-probability "foo" "foo")))
+  (is (= 0.92 (round (keyboard-distance-probability "test" "rwar"))))
+  (is (= 0.88 (round (keyboard-distance-probability "compile" "elipmoc"))))
+    ))
+
