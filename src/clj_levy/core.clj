62b767dc96f4879699e971c04d3a6518db0bc624
Folker Bernitt <folker-bernitt-github@gmx.de>
2013-07-30 20:12:15 +0200

    Initial leiningen setup.

---
+++ src/clj_levy/core.clj
@@ -0,0 +1,6 @@
+(ns clj-levy.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/clj_levy/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-levy.core-test
+  (:require [clojure.test :refer :all]
+            [clj-levy.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
